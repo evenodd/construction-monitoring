111dc8c6b6d665405d9e76ea404f413d6a1b5274
evenodd <shayneeven@gmail.com>
2020-06-14 14:16:51 +1000

    Percent completed fixup

--- react/src/util/JobUtil.js
+++ react/src/util/JobUtil.js
@@ -4,6 +4,6 @@ export default class JobUtil {
 
 JobUtil.calculateCompletedJobs = (jobs) => {
     const completed = jobs.filter(job => job.completed);
-    const pc = jobs.length && (completed.length / jobs.length);
+    const pc = jobs.length && (completed.length / jobs.length) * 100;
     return pc.toFixed(2);
 }
